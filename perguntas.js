f9e80590744556e510dbcd8340e09d97f9453a71
vieiranykolly <nykolly.vieira@escola.pr.gov.br>
2025-05-13 14:28:18 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Progrmação',
+    'O que é HTML?',
+    'Linguagem de marcação'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto e 2+2?',
+    '4'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    respota
+)
